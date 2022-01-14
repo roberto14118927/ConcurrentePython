8540686206f489e80e51ef785e6f70d51416c4fe
Roberto Guzman <robertoguzman@MacBook-Pro-de-Roberto-2.local>
2022-01-14 09:42:09 -0600

    iteraciones python

--- concurrente_01.py
+++ concurrente_01.py
@@ -28,15 +28,50 @@
 
 # # Listas
 
-lista = [1, 2, 3, 4, 5]
-lista = lista + [6, 7, 8, 9]
-lista += [10, 11, 12, 13]
+# lista = [1, 2, 3, 4, 5]
+# lista = lista + [6, 7, 8, 9]
+# lista += [10, 11, 12, 13]
 
-listaAnterior = ["A", "B", "C", "D", "E", "F"]
-listaNueva = ["G","H", "I", "J", "K", "L", "M"]
+# listaAnterior = ["A", "B", "C", "D", "E", "F"]
+# listaNueva = ["G","H", "I", "J", "K", "L", "M"]
 
-listaTotal = [*listaAnterior, *listaNueva]
+# listaTotal = [*listaAnterior, *listaNueva]
 
-listaTotal.append("O")
+# listaTotal.append("O")
 
-print(listaTotal)
+# print(listaTotal)
+
+# Sentencias y iteraciones
+# valorEntero = int(input("Ingrese numero"))
+
+# if valorEntero == 0:
+#     print("Es igual a cero")
+# elif valorEntero > 0:
+#     print("Es mayor a cero")
+# elif valorEntero == 10:
+#     print("Es mayor a cero")
+
+# listaNumeros = [1, 2, 3, 4, 5, 6, 7, 8]
+# for num in listaNumeros:
+#     print(num)
+
+# for i in range(30):
+#     print(i)
+
+# autos = { 'Chevrolet': True,  'Nissan': True, 'VMW': False, 'Jeep': False, 'Kia': True}
+# autosNuevos = {}
+
+# for marca, estatus in autos.items():
+#     if estatus == True:
+#         autosNuevos[marca] = estatus
+# print(autosNuevos)
+
+# while True:
+#     pass
+
+# def nueva_funcion(sumaUno):
+#     x = sumaUno + int("10")
+#     print('Resultado: ', x)
+
+
+# nueva_funcion(10)
